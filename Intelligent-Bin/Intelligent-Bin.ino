84cc55f18c3c22bc107ac5e7b2a49b9a09c4c3b5
Tommy <tommyteo38@gmail.com>
2021-01-09 19:01:32 +0800

    Add WiFi code, replace to non-blocking

--- Intelligent-Bin/Intelligent-Bin.ino
+++ Intelligent-Bin/Intelligent-Bin.ino
@@ -15,8 +15,6 @@
 
 // Constant Definition
 #define DEBUG true
-#define SSID ""
-#define PASSSWORD "" 
 
 // Global Variable Declaration
 const int redLEDPin = 6; // Red LED 
@@ -25,16 +23,22 @@ const int trigPin = 8; // Ultrasonic ranger: trigPin
 const int echoPin = 9; // Ultrasonic ranger: echoPin
 const int dipPins[4] = {13, 12, 11, 10};
 
+int errorSend = 0;
 int UID = 0;
+int greenLEDState = LOW;
 long distance;
 long height;
-String thingspeakAPI = "";
+long timeIntervalSendData = 1000; // 160 seconds
+unsigned long previousTimeBlinkLED = millis();
+unsigned long previousTimeSendData = millis();
+String thingspeakAPI = ""; // REPLACE WITH YOUR THINGSSPEAK API KEY
 
 // Class Declaration
 SoftwareSerial ESP01(2, 3); // RX, TX
 
 void setup() {
   Serial.begin(9600); // initialize serial communication:
+  ESP01.begin(9600); 
 
   pinMode(trigPin, OUTPUT); // UNO's output, ranger's input
   pinMode(echoPin, INPUT); // UNO's input, ranger's output
@@ -45,64 +49,43 @@ void setup() {
     pinMode(dipPins[i], INPUT_PULLUP);
   }
 
-  Serial.println("Setting up intelligent bin...");
-  delay(5000);
-
+  Serial.println("> Setting up intelligent bin...");
+  Serial.println("1. Calibrating intelligent bin's height");
+  delay(5000); // Wait for 5 Seconds for user to setup properly.
   height = measureDistance();
-  Serial.print(height);
-  Serial.println("cm");
+  // Serial.print(height);
+  // Serial.println("cm");
+
+  Serial.println("2. Connecting to Wi-Fi network");
+  connectWiFi();
+
+  Serial.println("> Intelligent bin has been sucessfully calibrated!");
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+
+  // Dip Switch Identifier
   UID = digitalRead(dipPins[0]) + ( digitalRead(dipPins[1]) * 2 ) + ( digitalRead(dipPins[2]) * 4 ) + ( digitalRead(dipPins[3]) * 8 );
-
-  long distance = measureDistance();
-  
-  Serial.print(distance);
-  Serial.println("cm");
-
-  controlLED(distance);
-
   // Serial.println();
   // Serial.print("UID: ");
   // Serial.println(UID);
 
-  sendData("AT+RST\r\n",2000,DEBUG);
-  sendData("AT+CWMODE=1\r\n",2000,DEBUG);
-  sendData("AT+CWJAP=\"SSID\",\"PASSWORD\"\r\n",4000,DEBUG);  
+  // Ultrasonic measure distance
+  long distance = measureDistance();
+  // Serial.print(distance);
+  // Serial.println("cm");
 
-  sendData("AT+CIPMUX=0\r\n",2000,DEBUG);
+  // LED switches
+  controlLED(currentTime, distance);
 
-  // Make TCP connection
-  String cmd = "AT+CIPSTART=\"TCP\",\"";
-  cmd += "3.208.241.190"; // Thingspeak.com's IP address  
-
-  cmd += "\",80\r\n";
-  sendData(cmd,2000,DEBUG);
-
-  // Prepare GET string
-  String getStr = "GET /update?api_key=";
-  getStr += thingspeakAPI;
-  getStr +="&field1=";
-  getStr += distance;
-  getStr += "\r\n"; // Last line for Line feed
-  
-  // Send data length & GET string
-  ESP01.print("AT+CIPSEND=");
-  ESP01.println (getStr.length());
-  Serial.print("AT+CIPSEND=");
-  Serial.println (getStr.length());  
-  delay(500);
-  if( ESP01.find( ">" ) )
-  {
-    Serial.print(">");
-    sendData(getStr,2000,DEBUG);
+  // Wi-Fi module, send data to ThingSpeak
+  if (currentTime-previousTimeSendData > timeIntervalSendData && !errorSend) {
+    previousTimeSendData = currentTime;
+    updateData(distance);
   }
 
-  // Close connection, wait a while before repeating...
-  sendData("AT+CIPCLOSE",16000,DEBUG); // thingspeak needs 15 sec delay between updates
-
-  delay(500);
+  delay(500); // 0.5s
 }
 
 long measureDistance() {
@@ -122,16 +105,24 @@ long measureDistance() {
   return distance;
 }
 
-void controlLED(int distance) {
+void controlLED(unsigned long currentTime, int distance) {
   digitalWrite(redLEDPin, LOW);
   digitalWrite(greenLEDPin, LOW);
 
-  if (distance < height * 0.15) {
-    digitalWrite(greenLEDPin, HIGH);
-    delay(1000);
-    digitalWrite(greenLEDPin, LOW);
+  if (distance < (height * 0.10)) {
+    // If bin is 90% filled, green LED blink
+    if (currentTime-previousTimeBlinkLED > 500) {
+      previousTimeBlinkLED = currentTime;
+      if (greenLEDState == HIGH) {
+        greenLEDState = LOW;
+      } else {
+        greenLEDState = HIGH;
+      }
+      digitalWrite(greenLEDPin, greenLEDState);
+    }
     Serial.println("Bin is full!");
-  } else if (distance < height * 0.25) {
+  } else if (distance < (height * 0.25)) {
+    // If bin is 75% filled, yellow LED on
     // digitalWrite(yellowLEDPin, HIGH);
     Serial.println("Bin is partially full.");
   } else {
@@ -139,23 +130,63 @@ void controlLED(int distance) {
   }
 }
 
-String sendData(String command, const int timeout, boolean debug)
-{
+void connectWiFi() {
+  sendCommand("AT+RST\r\n",2000,DEBUG); // Reset module
+  sendCommand("AT+CWMODE=1\r\n",2000,DEBUG); // Set station mode
+  sendCommand("AT+CWJAP=\"SSID\",\"PASSWORD\"\r\n",4000,DEBUG); // Connect Wi-Fi network (REPLACE WITH UR WIFI CREDENTIALS)
+  sendCommand("AT+CIPMUX=0\r\n",2000,DEBUG); // Set single-ip connection
+  Serial.println("> Wi-Fi Connected!");
+  sendCommand("AT+CIFSR\r\n",2000,DEBUG); // SHOW IP ADDRESS
+}
+
+void updateData(int distance) {
+  // Make TCP connection
+  String cmd = "AT+CIPSTART=\"TCP\",\"";
+  cmd += "54.210.227.170"; // Thingspeak.com's IP address  
+  cmd += "\",80\r\n";
+  sendCommand(cmd,2000,DEBUG);
+
+  // Prepare GET string
+  String getStr = "GET /update?api_key=";
+  getStr += thingspeakAPI;
+  getStr +="&field1=";
+  getStr += distance;
+  getStr += "\r\n"; // Last line for Line feed
+  
+  // Send data length & GET string
+  ESP01.print("AT+CIPSEND=");
+  ESP01.println(getStr.length());
+  delay(500);
+
+  if(ESP01.find(">")) {
+    // Serial.print("> ");
+    // Serial.println(getStr);
+    sendCommand(getStr,2000,DEBUG);
+  } else {
+    Serial.println("Error sending data, please check WiFi and restart...");
+    errorSend = 1;
+    // Close connection, wait a while before repeating...
+    // sendCommand("AT+CIPCLOSE",2000,DEBUG);
+    // connectWiFi();
+  }
+}
+
+String sendCommand(String command, const int timeout, boolean debug) {
     String response = "";
     ESP01.print(command);
     long int time = millis();
 
-    while( (time+timeout) > millis()) {
+    while((time+timeout) > millis()) {
       while(ESP01.available()) {
         // "Construct" response from ESP01 as follows 
          // - this is to be displayed on Serial Monitor. 
         char c = ESP01.read(); // read the next character.
-        response+=c;
+        response += c;
       }  
     }
 
     if(debug) {
-      Serial.print(response);
+      Serial.println(response);
     }
     
     return (response);
